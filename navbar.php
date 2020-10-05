1f84c9ebcb9aa1f537868a7b4a06b8b555ef1a1c
ncatacmic <72328253+ncatacmic@users.noreply.github.com>
2020-10-05 10:32:06 -0400

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-expand-lg">
+    <a class="navbar-brand" href="index.html">NCAT ACM Interview Prep</a>
+    <button
+      class="navbar-toggler navbar-dark"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.html"
+            ><span class="nav-link-num">01.</span> Home
+            <span class="sr-only">(current)</span></a
+          >
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="resources.html"
+            ><span class="nav-link-num">02.</span> Resources</a
+          >
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="members.html"
+            ><span class="nav-link-num">03.</span> Members</a
+          >
+        </li>
+      </ul>
+      <div class="form-inline my-2 my-lg-0">
+        <a class="btn" target="_blank" href="http://csacm.ncat.edu/index.html">NCAT ACM</a>
+        <a target="_blank" href="http://instagram.com/acm.ncat">
+          <i class="fa fa-instagram"></i>
+        </a>
+      </div>
+    </div>
+  </nav>
